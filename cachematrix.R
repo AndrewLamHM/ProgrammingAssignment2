6b4e95ddfc470895004aa337d5c29f33495d90a5
AndrewLamHM <lamhoming.andrew@gmail.com>
2017-10-18 11:22:12 +0800

    This is for re-submission on 18 OCT 1130#

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,6 @@
-## Put comments here that give an overall description of what your
-## functions do
-## Write a short comment describing this function
+## return a list of get set
+## INPUT - a square matrix 
+## OUTPUT 4 elements - a.Set Matrix ;b.Get Matrix ;c.setInverse Matrix ;d.getInverseMatrix
 makeCacheMatrix <- function(x = matrix()) {
     invm <- NULL
     set <- function(y) {
@@ -15,15 +15,42 @@ makeCacheMatrix <- function(x = matrix()) {
          getinverse = getinverse)
 }
 
-## Write a short comment describing this function
-cacheSolve <- function(x, ...) { ## Return a matrix that is the inverse of 'x'
+## Return a matrix that is the inverse input matrix x
+## INPUT - a square matrix
+## OUTPUT - the inverse matrix of INPUT
+cacheSolve <- function(x, ...) { 
     invm <- x$getinverse()
-    if(!is.null(invm)) {
+    ##if inverse exists, direct return that
+    if(!is.null(invm)) {  
         message("getting cached data")
         return(invm)
     }
-    data <- x$get()
+    ##get inverse matrix of INPUT, set inverse matrix if that's not exist
+    data <- x$get() 
     invm <- solve(data, ...)
     x$setinverse(invm)
     invm
 }
+##> A <- matrix(c(1,1,4,0,3,1,4,4,0), nrow = 3, ncol = 3)
+##> vA <- makeCacheMatrix(A)
+##> vA$get()
+##[,1] [,2] [,3]
+##[1,]    1    0    4
+##[2,]    1    3    4
+##[3,]    4    1    0
+##> cacheSolve(vA)
+##[,1]        [,2]    [,3]
+##[1,]  0.08333333 -0.08333333  0.2500
+##[2,] -0.33333333  0.33333333  0.0000
+##[3,]  0.22916667  0.02083333 -0.0625
+##> vA$getinverse()
+##[,1]        [,2]    [,3]
+##[1,]  0.08333333 -0.08333333  0.2500
+##[2,] -0.33333333  0.33333333  0.0000
+##[3,]  0.22916667  0.02083333 -0.0625
+##> cacheSolve(vA)
+##getting cached data
+##[,1]        [,2]    [,3]
+##[1,]  0.08333333 -0.08333333  0.2500
+##[2,] -0.33333333  0.33333333  0.0000
+##[3,]  0.22916667  0.02083333 -0.0625
